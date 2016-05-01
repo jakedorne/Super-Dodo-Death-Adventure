0242faf30a19ec6e3849a29fe8146f79d65274e8
Matt Byers <matt_byers@live.com>
2016-05-01 20:45:13 +1200

    Dodo's spawning + some of movement done [broken]
    Dodo's spawining on the first block in the floor grid, model origin is
    incorrect so it spawns off center a bit. Also fly's of the screen
    instantly.. not sure why? Wrote first movement algorithm, didn't work at
    all, will look at it again tomorrow.

---
+++ Assets/Scripts/DodoBehaviour.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DodoBehaviour : MonoBehaviour {
+
+	private GameObject floor;
+	private Floor floorScript;
+
+	int currentX;
+	int currentZ;
+
+	int lastX;
+	int lastZ;
+
+
+	// Use this for initialization
+	void Start () {
+		floor = GameObject.Find ("Floor");
+		floorScript = floor.GetComponent<Floor> ();
+		currentX = floorScript.startX;
+		currentZ = floorScript.startZ;
+
+		//InvokeRepeating ("moveDodo", 2f, 2f);
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+			
+
+	}
+
+	//These methods are not currently working, hence they are commented out to avoid console spam.
+	private void moveDodo() {
+		Dictionary<Vector2, Vector3> potentialBlocks = new Dictionary<Vector2, Vector3> ();
+
+		Vector2 left = floorScript.getCoordAtVector (transform.right * -1);
+		Vector2 right = floorScript.getCoordAtVector (transform.right);
+		Vector2 forward = floorScript.getCoordAtVector (transform.forward);
+
+		if (floorScript.positionOnBlock ((int)left.x, (int)left.y)) potentialBlocks.Add (left, transform.right * -1);
+		if (floorScript.positionOnBlock ((int)right.x, (int)right.y)) potentialBlocks.Add (right, transform.right);
+		if (floorScript.positionOnBlock ((int)forward.x, (int)forward.y)) potentialBlocks.Add (forward, transform.forward);
+
+		Vector2 bestBlock = findBestBlock (potentialBlocks);
+
+		if (bestBlock == left) {
+			transform.Rotate (0, 270, 0);
+		} else if (bestBlock == right) {
+			transform.Rotate (0, 90, 0);
+		}
+
+		transform.position = potentialBlocks [bestBlock];
+	}
+
+	private Vector2 findBestBlock( Dictionary<Vector2, Vector3> potentialBlocks) {
+		Vector2 bestGridPos = Vector2.zero;
+		Vector3 bestWorldPos = Vector3.zero;
+
+		Vector3 goalWorldPos = floorScript.getVectorAtCoords (floorScript.endX, floorScript.endZ);
+
+		foreach (Vector2 gridPos in potentialBlocks.Keys) {
+			if (bestGridPos == Vector2.zero) {
+				bestGridPos = gridPos;
+				bestWorldPos = potentialBlocks [gridPos];
+			}
+			else if (Vector3.Distance (potentialBlocks[gridPos], goalWorldPos) < Vector3.Distance (bestWorldPos, goalWorldPos)) {
+				bestGridPos = gridPos;
+				bestWorldPos = potentialBlocks [gridPos];
+			}
+		}
+
+		return bestGridPos;
+	}
+		
+}
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -3,13 +3,38 @@ using System.Collections;
 
 public class Spawner : MonoBehaviour {
 
+	public GameObject dodoPrefab;
+	public int dodosToSpawn;
+
+	private int dodoCount;
+	private GameObject[] dodoList;
+	private GameObject dodo;
+	private GameObject floor;
+
+
+	private int startX;
+	private int startZ;
+
+
 	// Use this for initialization
 	void Start () {
-	
+		dodoList = new GameObject [dodosToSpawn];
+		floor = GameObject.Find("Floor");
+		startX = floor.GetComponent<Floor> ().startX;
+		startZ = floor.GetComponent<Floor> ().startZ;
+
+		InvokeRepeating ("spawnDodo", 0f, 5f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
 	
+	}
+
+	void spawnDodo() {
+		dodoList[dodoCount] = (GameObject)Instantiate (dodoPrefab, floor.GetComponent<Floor> ().getVectorAtCoords (startX, startZ), Quaternion.identity);
+		dodoCount++;
+		if (dodoCount >= dodosToSpawn) CancelInvoke ("spawnDodo");
 	}
 }
