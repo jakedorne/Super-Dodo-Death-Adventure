d91c77e48d3541d712ab655868ff34e248c573d9
Matt Byers <matt_byers@live.com>
2016-05-12 20:48:35 +1200

    Dodo move still broken, nothing to see here.

--- Assets/Scripts/DodoBehaviour.cs
+++ Assets/Scripts/DodoBehaviour.cs
@@ -13,6 +13,10 @@ public class DodoBehaviour : MonoBehaviour {
 	int lastX;
 	int lastZ;
 
+	Vector3 currentRotation;
+
+	int count = 0;
+
 
 	// Use this for initialization
 	void Start () {
@@ -20,6 +24,7 @@ public class DodoBehaviour : MonoBehaviour {
 		floorScript = floor.GetComponent<Floor> ();
 		currentX = floorScript.startX;
 		currentZ = floorScript.startZ;
+		currentRotation = transform.rotation.eulerAngles;
 
 		InvokeRepeating ("moveDodo", 2f, 2f);
 
@@ -36,45 +41,64 @@ public class DodoBehaviour : MonoBehaviour {
 	//These methods are not currently working.
 	private void moveDodo() {
 		float offset = 12f;
-		Dictionary<Vector2, Vector3> potentialBlocks = new Dictionary<Vector2, Vector3> ();
 
-		Vector2 left = floorScript.getCoordAtVector (transform.right * -1)/offset;
-		Vector2 right = floorScript.getCoordAtVector (transform.right)/offset;
-		Vector2 forward = floorScript.getCoordAtVector (transform.forward)/offset;
 
-		if (floorScript.positionOnBlock ((int)left.x, (int)left.y)) potentialBlocks.Add (left, (transform.right * -1)/offset);
-		if (floorScript.positionOnBlock ((int)right.x, (int)right.y)) potentialBlocks.Add (right, (transform.right)/offset);
-		if (floorScript.positionOnBlock ((int)forward.x, (int)forward.y)) potentialBlocks.Add (forward, (transform.forward)/offset);
+		List<Vector2> potentialBlocks = new List<Vector2> ();
+
+		if(floorScript.positionOnBlock(currentX+1, currentZ)) potentialBlocks.Add(new Vector2(currentX+1, currentZ));
+		if(floorScript.positionOnBlock(currentX-1, currentZ))potentialBlocks.Add(new Vector2(currentX-1, currentZ));
+		if(floorScript.positionOnBlock(currentX, currentZ+1))potentialBlocks.Add(new Vector2(currentX, currentZ+1));
+		if(floorScript.positionOnBlock(currentX, currentZ-1))potentialBlocks.Add(new Vector2(currentX, currentZ-1));
+
+		//if (floorScript.positionOnBlock ((int)left.x, (int)left.y)) potentialBlocks.Add (left, (transform.right * -1)/offset);
+		//if (floorScript.positionOnBlock ((int)right.x, (int)right.y)) potentialBlocks.Add (right, (transform.right)/offset);
+		//if (floorScript.positionOnBlock ((int)forward.x, (int)forward.y)) potentialBlocks.Add (forward, (transform.forward)/offset);
+
+		potentialBlocks = removeLastPos (potentialBlocks);
 
 		if (potentialBlocks.Count == 0)
 			return;
 
 		Vector2 bestBlock = findBestBlock (potentialBlocks);
 
-		if (bestBlock == left) {
-			transform.Rotate (0, 270, 0);
-		} else if (bestBlock == right) {
-			transform.Rotate (0, 90, 0);
+		//if (bestBlock == left) {
+			//transform.Rotate (0, 270, 0);
+		//} else if (bestBlock == right) {
+			//transform.Rotate (0, 90, 0);
+		//}
+
+		lastX = currentX;
+		lastZ = currentZ;
+		currentX = (int)bestBlock.x;
+		currentZ = (int)bestBlock.y;
+
+		transform.position = floorScript.getVectorAtCoords((int)bestBlock.x, (int)bestBlock.y);
+	}
+	
+	public List<Vector2> removeLastPos(List<Vector2> potentialBlocks) {
+		Vector2 toRemove = Vector2.zero;
+		foreach (Vector2 gridPos in potentialBlocks) {
+			if ((int)gridPos.x == lastX && (int)gridPos.y == lastZ) {
+				toRemove = gridPos;
+				break;
+			}
 		}
 
-		transform.position = potentialBlocks [bestBlock];
+		if(toRemove != Vector2.zero) potentialBlocks.Remove (toRemove);
+		return potentialBlocks;
 	}
+	
 
 	//Could be the problem
-	private Vector2 findBestBlock( Dictionary<Vector2, Vector3> potentialBlocks) {
+	private Vector2 findBestBlock(List<Vector2> potentialBlocks) {
 		Vector2 bestGridPos = Vector2.zero;
-		Vector3 bestWorldPos = Vector3.zero;
+		Vector2 goalGridPos = new Vector2 (floorScript.endX, floorScript.endZ);
 
-		Vector3 goalWorldPos = floorScript.getVectorAtCoords (floorScript.endX, floorScript.endZ);
-
-		foreach (Vector2 gridPos in potentialBlocks.Keys) {
+		foreach (Vector2 gridPos in potentialBlocks) {
 			if (bestGridPos == Vector2.zero) {
 				bestGridPos = gridPos;
-				bestWorldPos = potentialBlocks [gridPos];
-			}
-			else if (Vector3.Distance (potentialBlocks[gridPos], goalWorldPos) < Vector3.Distance (bestWorldPos, goalWorldPos)) {
+			} else if (Vector3.Distance (gridPos, goalGridPos) < Vector3.Distance (bestGridPos, goalGridPos)) {
 				bestGridPos = gridPos;
-				bestWorldPos = potentialBlocks [gridPos];
 			}
 		}
 
