b742a963f24210ecb594f49f80641a1aa17c869e
MichaelMillward <michael.millward.nz@gmail.com>
2016-05-24 22:14:23 +1200

    added the block buttons

--- Assets/Scripts/InventoryButton.cs
+++ Assets/Scripts/InventoryButton.cs
@@ -7,18 +7,27 @@ public class InventoryButton : MonoBehaviour {
 	public TetrisBlock.Shape value;
 	public int amount;
 
+	const string UNCLICKED_EXTENSION = "0";
+	const string CLICKED_EXTENSION = "1";
+
 	// Use this for initialization
 	void Start () {
 		GameObject managerGO = GameObject.FindGameObjectWithTag ("LevelManager");
 		LevelManager manager = managerGO.GetComponent<LevelManager> ();
-		// Set face value
-		GetComponent<Button> ().transform.GetChild (0).GetComponent<Text> ().text = value.ToString() + ":" + amount;
+		// Set face image
+		string filename = value.ToString() + UNCLICKED_EXTENSION;
+		Sprite sprite = Resources.Load<Sprite>(filename);
+		GetComponent<Image> ().sprite = sprite;
+		GetComponent<Button> ().transform.GetChild (0).GetComponent<Text> ().text = "" + amount;
 	}
 
 	public void buttonClicked(){
 		GameObject managerGO = GameObject.FindGameObjectWithTag ("LevelManager");
 		LevelManager manager = managerGO.GetComponent<LevelManager> ();
-		GetComponent<Button> ().interactable = false;
+		string filename = value.ToString() + CLICKED_EXTENSION;
+		Sprite sprite = Resources.Load<Sprite>(filename);
+		GetComponent<Image> ().sprite = sprite;
 		manager.addTile (value);
 	}
+		
 }
