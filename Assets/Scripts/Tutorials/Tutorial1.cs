c0f3a29e4c06c5c03819a674417c3a96ce319343
MichaelMillward <michael.millward.nz@gmail.com>
2016-06-08 20:05:17 +1200

    refactoring tutorial ui

---
+++ Assets/Scripts/Tutorials/Tutorial1.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tutorial1 : TutorialAbstract {
+
+	int numberOfLessons = 3;
+
+	// Different lessons
+	const int SelectionLessonID = 0;
+	const int RotationLessonID = 1;
+	const int PlacementLessonID = 2;
+
+	// Lesson Texts are defined in here for now, might move into a text file
+	string SelectionLesson = "To get started, select a block from your inventory.\n\nThis can be done by selecting the block with the mouse, or you can iterate through your inventory by right clicking the mouse or using the W and S keys.";
+	string RotationLesson = "Try rotating the block.\n\nYou can do this by using the scroll wheel on your mouse, or using the A and D keys.";
+	string PlacementLesson = "Now try place the block.\n\nWhen you have positioned it where you would like to place it, left click on the mouse.";
+
+	bool[] completeLessons;
+	string[] lessons;
+
+	public override void intialise(){
+		completeLessons = new bool[numberOfLessons];
+		lessons = new string[numberOfLessons];
+		for (int i = 0; i < numberOfLessons; i++) {
+			completeLessons [i] = false;
+			if (SelectionLessonID == i) {
+				lessons [i] = SelectionLesson;
+			}
+			else if (RotationLessonID == i) {
+				lessons [i] = RotationLesson;
+			}
+			else if (PlacementLessonID == i) {
+				lessons [i] = PlacementLesson;
+			}
+		}
+	}
+
+	public override void updateLessonState ()
+	{
+		// Check to see if block has been rotated. Then the block rotation lesson 
+		// does not need to occur.
+		if (base.manager.getCurrentBlock() != null && manager.getCurrentBlock ().hasBeenRotated) {
+			completeLessons [RotationLessonID] = true;
+		}
+		// Check to see if the user has selected a block, if so, block selection lesson
+		// does not need to occur.
+		if (manager.getCurrentBlock() != null){
+			completeLessons [SelectionLessonID] = true;
+		}
+
+		if(manager.getNumberOfBlocksPlaced() > 0) {
+			completeLessons [PlacementLessonID] = true;
+		}
+	}
+
+	public override string getNextLesson(){
+		for (int i = 0; i < numberOfLessons; i++) {
+			if (completeLessons [i] == false) {
+				return lessons [i];
+			}
+		}
+		return null;
+	}
+
+	public override bool isLessonComplete (string lesson)
+	{
+		for(int i = 0; i < noOfLessons; i++){
+			if (lessons [i] == lesson && completeLessons[i]) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+		
+}
---
+++ Assets/Scripts/Tutorials/TutorialAbstract.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public abstract class TutorialAbstract : MonoBehaviour {
+
+	public GameObject lessonScreenPrefab;
+	public GameObject infoObject;
+	public GameObject lessonPlaceholder;
+
+	const float DELAYONLESSON = 2.0f;
+
+	public int noOfLessons = 0;
+
+	GameObject levelScreen;
+	float timeLeftBeforeLesson;
+	bool timerOn;
+	public LevelManager manager;
+	string currentLesson;
+
+	// Abstract methods
+	abstract public void updateLessonState();
+	abstract public string getNextLesson ();
+	abstract public bool isLessonComplete (string lesson);
+	abstract public void intialise();
+
+	// Use this for initialization
+	void Start () {
+		timeLeftBeforeLesson = DELAYONLESSON;
+		timerOn = true;
+		manager = FindObjectOfType<LevelManager> ();
+		intialise ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		updateLessonState ();
+		if (currentLesson != null && isLessonComplete (currentLesson)) {
+			prepareNextLesson ();
+			currentLesson = null;
+		}
+		if (timerOn) {
+			timeLeftBeforeLesson -= Time.deltaTime;
+			if (timeLeftBeforeLesson < 0) {
+				presentLesson ();
+				timerOn = false;
+			}
+		}
+	}
+
+	public void presentLesson(){
+		if (getNextLesson () == null) {
+			completeTutorial ();
+		}
+		else{
+			levelScreen = Instantiate (lessonScreenPrefab) as GameObject;
+			levelScreen.transform.SetParent (transform);
+			levelScreen.GetComponent<LessonScreen> ().SetPosition (lessonPlaceholder.transform.position);
+			levelScreen.GetComponent<LessonScreen> ().StartBobbing (LessonScreen.Axis.y);
+			Transform screensChild = levelScreen.transform.GetChild (0); // get text
+			currentLesson = getNextLesson ();
+			screensChild.GetComponent<Text>().text = currentLesson;
+			// notify user
+			string filename = "info1";
+			Sprite sprite = Resources.Load<Sprite>(filename);
+			infoObject.GetComponent<Image> ().sprite = sprite;
+		}
+	}
+
+	public void completeTutorial(){
+		Destroy (levelScreen);
+		FindObjectOfType<Spawner> ().beginSpawning();
+
+		// disable notification
+		string filename = "info0";
+		Sprite sprite = Resources.Load<Sprite>(filename);
+		infoObject.GetComponent<Image> ().sprite = sprite;
+
+		// ensure timer is off
+		timerOn = false;
+	}
+
+	public void prepareNextLesson(){
+		print ("Prepare next lesson");
+		Destroy (levelScreen);
+		timerOn = true;
+		timeLeftBeforeLesson = DELAYONLESSON;
+
+		// disable notification
+		string filename = "info0";
+		Sprite sprite = Resources.Load<Sprite>(filename);
+		infoObject.GetComponent<Image> ().sprite = sprite;
+	}
+
+
+}
